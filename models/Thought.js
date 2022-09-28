96b26e9fc1cd639d3dc627302321d82685ea90f8
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-09-27 20:29:57 -0600

    modified regex in thought js

--- models/Thought.js
+++ models/Thought.js
@@ -6,7 +6,7 @@ const thoughtSchema = new Schema(
         thoughtText:{
             type:String,
             required:true,
-            match:[/^[]{1,280}$/,'Thoughts must be between 1 and 280 characters']
+            match:[/^[.]{1,280}$/,'Thoughts must be between 1 and 280 characters']
         },
         createdAt:{
             type: Date,
