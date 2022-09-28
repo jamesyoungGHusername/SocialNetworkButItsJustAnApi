fccfc7d1ade74f048359dc4f1c122dfd2849a76d
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-09-27 20:29:26 -0600

    modified routing for adding friends, tested userController, modified regex to apply check to any characters.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,6 +11,6 @@ const {
 
   router.route('/').get(getThoughts).post(createThought);
   router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
-  router.route('/api/thoughts/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
+  router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
 
   module.exports = router;
